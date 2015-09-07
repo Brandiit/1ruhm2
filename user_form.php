c7c6a11ed046fcfc718b165746adb06fa179b3e2
Brendon Brant <brenbra@greeny.cs.tlu.ee>
2015-09-07 15:42:35 +0300

    control

--- user_form.php
+++ user_form.php
@@ -1,11 +1,21 @@
 <?php
 
 echo $_POST["email"];
-
-	//Tühjuse kontroll
-	if(empty($_POST["email"])) {
-		$email_error ="Unavailable";
-	if($_SERVER["REQUEST_METHOD"] == "POST");
+	$email_error = "";
+echo $_POST["password"];
+	$password_error ="";
+	
+	if($_SERVER["REQUEST_METHOD"] == "POST"){
+		
+		//Tühjuse kontroll
+		if(empty($_POST["email"])) {
+			$email_error ="Unavailable";
+			if(empty($_POST["password"])) {	
+				$password_error = "Also unavailable";
+			}
+			
+		}
+	
 	}
 ?>
 <html>
@@ -17,10 +27,10 @@ echo $_POST["email"];
 	<h2>Login</h2>
 	<form action="user_form.php" method="post">
 	<input name="email" type="email" placeholder="E-mail"> <?php echo $email_error; ?> <br>
-	<input name="password" type="password" placeholder="Password"><br>
+	<input name="password" type="password" placeholder="Password"> <?php echo $password_error; ?> <br>
 	<input type="submit" value="Log in"> 
 	</form>
 	<h2>Create user</h2>
 
-		</body>
-</html>
+	</body>
+</html> 
