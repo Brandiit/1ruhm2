f6d20270d6cd3f3debe6a187a4314cf4e5762ec8
Brendon Brant <brenbra@greeny.cs.tlu.ee>
2015-09-07 14:57:58 +0300

    user

---
+++ user_form.php
@@ -0,0 +1,22 @@
+<?php
+
+
+
+?>
+<html>
+	<head>
+		<title> Login </title>
+	</head>
+	<body>
+	
+	<h2>Login</h2>
+	<input type="email" placeholder="E-mail"><br>
+	<br>
+	<input type="password" placeholder="Password"><br>
+	
+	<input type="submit" value="Log in">
+
+	<h2>Create user</h2>
+
+		</body>
+</html>
