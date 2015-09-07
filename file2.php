6ee8134fdb170e1262c8f5e1991b6d296814803e
Brendon Brant <brenbra@greeny.cs.tlu.ee>
2015-09-07 14:40:17 +0300

    date

---
+++ file2.php
@@ -0,0 +1,34 @@
+<?php
+
+	$first_name = "Brandiit";
+	$last_name = "Marauder";
+
+	echo $first_name." ".$last_name;
+
+?>
+<br>
+<?php
+
+	$age = 5
+
+	($age < 18) {
+	echo "alaealine";
+	} else {
+	echo "täisealine";
+	}
+
+?>
+
+<br>
+
+<?php
+
+	for($i = 0; $i < $age; $i = $i + 1) {
+	echo "palju".$i.",;
+	echo "õnne;" ";
+	}
+?>
+<br>
+<?php
+
+	echo date("l, ")
